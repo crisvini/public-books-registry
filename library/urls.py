97c1c6f6b3cff580c5e4fe0b7b97a72919659ffd
Cristian <crisvini.leoncini@outlook.com>
2024-01-22 15:26:45 -0300

    tela novo

--- library/urls.py
+++ library/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, novo
 
 urlpatterns = [
     path('', index),
+    path('novo', novo),
 ]
--- library/views.py
+++ library/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'library/index.html')
+
+
+def novo(request):
+    return render(request, 'library/novo.html')
